eef0705fc15eff212b8097f2f886607d2f4d0dc1
cillanguyen <cillangu@uw.edu>
2023-07-28 01:32:10 -0700

    finish making changs to experiences

--- script.js
+++ script.js
@@ -28,4 +28,24 @@ document.addEventListener('DOMContentLoaded', function () {
   // Update lines position when the page loads and when it's scrolled
   updateLinesPosition();
   window.addEventListener('scroll', updateLinesPosition);
+
+  // New event listener for .app-skills-exp-item elements
+  const appSkillsItems = document.querySelectorAll(".app-skills-exp-item");
+
+  appSkillsItems.forEach((item) => {
+    const tooltipText = item.getAttribute("data-tooltip");
+
+    if (tooltipText) {
+      item.addEventListener("mouseover", () => {
+        const tooltip = document.createElement("div");
+        tooltip.classList.add("tooltip");
+        tooltip.innerText = tooltipText;
+        item.appendChild(tooltip); // Append the tooltip to the .app-skills-exp-item
+
+        item.addEventListener("mouseout", () => {
+          item.removeChild(tooltip); // Remove the tooltip when the mouse leaves the .app-skills-exp-item
+        });
+      });
+    }
+  });
 });
